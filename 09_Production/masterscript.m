5f644bf29a95a94c79e4ccd82c52c430416f7733
Magnus <magnus_wangensteen@hotmail.com>
2020-12-26 11:05:59 +0100

    Production ready

--- 09_Production/masterscript.m
+++ 09_Production/masterscript.m
@@ -1,6 +1,8 @@
 formatSpec = '%03d';
 FileName = 'X:\Magnus\PhD\Matlab_data\09_Production\data_full.mat';
-for thickness = 64:-1:63
+FileNameDecim = 'data_decim.mat';
+FileNameDecimBackup = 'X:\Magnus\PhD\Matlab_data\09_Production\data_decim.mat';
+for thickness = 62:-1:61 %complete 64:-1:63
     for velcoeff=0.96:0.02:1.04
         fieldname = ['d' num2str(thickness, formatSpec) '_' num2str(0, formatSpec) '_' num2str(0, formatSpec) '_' num2str(velcoeff*100, formatSpec)];
     %     A_decim.(fieldname) = 52;
@@ -14,7 +16,19 @@ for thickness = 64:-1:63
             disp(['Creating file ' FileName]);
         end
         clear data_full
-        for pitting_depth = 2:2:4
+        if exist(FileNameDecim, 'file')
+            save(FileNameDecim, '-struct', 'data_decim', fieldname, '-append');
+        else
+            save(FileNameDecim, '-struct', 'data_decim', fieldname);
+            disp(['Creating file ' FileNameDecim]);
+        end
+        if exist(FileNameDecimBackup, 'file')
+            save(FileNameDecimBackup, '-struct', 'data_decim', fieldname, '-append');
+        else
+            save(FileNameDecimBackup, '-struct', 'data_decim', fieldname);
+            disp(['Creating file ' FileNameDecimBackup]);
+        end
+        for pitting_depth = 2:2:12 % complete 2:2:4
             for seedi = 0:1:2
                 fieldname = ['d' num2str(thickness, formatSpec) '_' num2str(pitting_depth, formatSpec) '_' num2str(seedi, formatSpec) '_' num2str(velcoeff*100, formatSpec)];
     %             disp(fieldname);
@@ -25,6 +39,8 @@ for thickness = 64:-1:63
     %             save
                 save(FileName, '-struct', 'data_full', fieldname, '-append');
                 clear data_full
+                save(FileNameDecim, '-struct', 'data_decim', fieldname, '-append');
+                save(FileNameDecimBackup, '-struct', 'data_decim', fieldname, '-append');
             end
         end
     end
